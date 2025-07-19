9826e937ea6d5d40568e9cece8b1a06a1605476a
NoxMonnzReal <monnzfixz@gmail.com>
2025-07-19 20:12:39 +0700

    Update design.sh

--- design.sh
+++ design.sh
@@ -13,11 +13,10 @@ echo "-  Module Name    =   NoxXB - PRO "
 sleep 1
 
 # Checking Internet
-if ping -c 1 8.8.8.8 &> /dev/null
-then
-  echo "-  Module Proper  =   Connected "
+if ping -c 1 8.8.8.8 >/dev/null 2>&1; then
+  echo "-  Module Proper  = Connected "
 else
-  echo "-  Module Proper  =   Non Connected! "
+  echo "-  Module Proper  = Non Connected! "
   exit 1
 fi
 
