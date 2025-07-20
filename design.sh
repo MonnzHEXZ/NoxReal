4bb9c784054f048db721e2a5b600ad3173cc0698
NoxMonnzReal <monnzfixz@gmail.com>
2025-07-20 10:17:03 +0700

    Update design.sh

--- design.sh
+++ design.sh
@@ -11,15 +11,7 @@ echo "-  Version        =   V1.4.5 PRO "
 sleep 0.5
 echo "-  Module Name    =   NoxXB - PRO "
 sleep 1
-
-# Checking Internet
-if ping -c 1 8.8.8.8 >/dev/null 2>&1; then
-  echo "-  Module Proper  =   Connected "
-else
-  echo "-  Module Proper  =   Non Connected! "
-  exit 1
-fi
-
+echo "-  Module Proper  =   Connection Required "
 echo " "
 sleep 3
 }
